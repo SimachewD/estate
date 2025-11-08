9cc4a67fa1b2631b415233b085481bd6678455b9
SimachewD <simachewdenekewse@gmail.com>
2025-11-08 16:07:57 +0300

    initial code for odoo module - estate

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Real Estate Management',
+    'version': '1.0',
+    'summary': 'Manage real estate properties, their details and sales',
+    'description': """
+        Real Estate Management Module
+        - Track properties
+        - Record details such as price, bedrooms, garden, garage, etc.
+        - Plan availability and sales
+        """,
+    'author': 'Odoo-16-training',
+    'category': 'Real Estate',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
